34dc2a83f425ffe1a0041b68ae3b55a3a80f03dc
Alex Shornal <shornality@yahoo.com>
2018-02-01 22:43:53 -0500

    added personal page

--- index.php
+++ index.php
@@ -5,11 +5,20 @@
  * Date: 1/17/2018
  * Time: 9:12 PM
  */
+session_start();
+
+error_reporting(E_ALL);
+ini_set('display_errors', 3);
+
 //Require the autoload file
 require_once('vendor/autoload.php');
 
 //Create an instance of the Base Class
 $f3 = Base::instance();
+
+//set debug level
+$f3->set('DEBUG', 3);
+
 
 //Define a default route
 $f3->route('GET /', function()
@@ -20,6 +29,52 @@ $f3->route('GET /', function()
     ('pages/home.html');
 }
 );
+$f3->route("GET /pages/personal", function()
+{
+    $template = new Template();
+    echo $template->render('pages/personal.html');
+}
+);
+$f3->route("POST /pages/profile", function()
+{
+    $template = new Template();
+    echo $template->render('pages/profile.html');
+    $_SESSION['animal'] = $_POST['animal'];
+}
+);
+/*
+$f3->route("POST /pets/results", function()
+{
+    $_SESSION['color'] = $_POST['color'];
+    echo "<h1>Results Page</h1>";
+    echo "Thank you for ordering a ".$_SESSION['color']. " ".$_SESSION['animal'];
+}
+);
+
+$f3->route("GET|POST /new-pet", function($f3)
+{
+
+
+    if(isset($_POST['submit'])) {
+
+        $color = $_POST['pet-color'];
+        $type = $_POST['pet-type'];
+        $name = $_POST['pet-name'];
+
+
+        include ('model/validate.php');
+
+    }
+    $f3->set('color', $color);
+    $f3->set('type', $type);
+    $f3->set('name', $name);
+    $f3->set('errors', $errors);
+    $f3->set('success', $success);
+
+    $template = new Template();
+    echo $template->render('views/new-pet.html');
+}
+);*/
 
 //Run Fat-Free
 $f3->run();
