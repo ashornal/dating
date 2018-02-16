b36e365943c68574f56a5fa02b2f0f62dcf0d22c
Alex Shornal <shornality@yahoo.com>
2018-02-15 21:01:27 -0500

    added docblock

--- classes/Member.php
+++ classes/Member.php
@@ -6,6 +6,9 @@
  * Time: 4:48 PM
  */
 
+/**
+ * Class Member represents a member of the dating site
+ */
 class Member
 {
     protected $fname;
@@ -18,6 +21,14 @@ class Member
     protected $seeking;
     protected $bio;
 
+    /**
+     * Member constructor.
+     * @param $fname first name of user
+     * @param $lname last name of user
+     * @param $age age of user
+     * @param $gender gender of user
+     * @param $phone phone number of user
+     */
     public function __construct($fname, $lname, $age, $gender, $phone)
     {
         $this->fname = $fname;
@@ -28,92 +39,137 @@ class Member
     }
 
     //Setters & Getters
-
+    /**
+     * Sets the first name of the user
+     * @param $fname first name
+     */
     function setFName($fname)
     {
         $this->fname = $fname;
     }
-
+    /**
+     * @return first name of user
+     */
     function getFName()
     {
         return $this->fname;
     }
-
+    /**
+     * Sets the last name of the user
+     * @param $lname last name
+     */
     function setLName($lname)
     {
         $this->lname = $lname;
     }
-
+    /**
+     * @return last name of user
+     */
     function getLName()
     {
         return $this->lname;
     }
-
+    /**
+     * Sets the age of the user
+     * @param $age age of user
+     */
     function setAge($age)
     {
         $this->age = $age;
     }
-
+    /**
+     * @return age of user
+     */
     function getAge()
     {
         return $this->age;
     }
-
+    /**
+     * Sets the gender of the user
+     * @param $gender gender of user
+     */
     function setGender($gender)
     {
         $this->gender = $gender;
     }
-
+    /**
+     * @return gender of user
+     */
     function getGender()
     {
         return $this->gender;
     }
-
+    /**
+     * Sets the phone number of the user
+     * @param $phone phone number of user
+     */
     function setPhone($phone)
     {
         $this->phone = $phone;
     }
-
+    /**
+     * @return phone number of user
+     */
     function getPhone()
     {
         return $this->phone;
     }
-
+    /**
+     * Sets the email of the user
+     * @param $email email of user
+     */
     function setEmail($email)
     {
         $this->email = $email;
     }
-
+    /**
+     * @return email of user
+     */
     function getEmail()
     {
         return $this->email;
     }
-
+    /**
+     * Sets the state location of user
+     * @param $state state of user
+     */
     function setState($state)
     {
         $this->state = $state;
     }
-
+    /**
+     * @return state of the user
+     */
     function getState()
     {
         return $this->state;
     }
-
+    /**
+     * Set what gender the user is seeking
+     * @param $seeking gender user is seeking
+     */
     function setSeeking($seeking)
     {
         $this->seeking = $seeking;
     }
-
+    /**
+     * @return gender user is seeking
+     */
     function getSeeking()
     {
         return $this->seeking;
     }
-
+    /**
+     * Sets the biography of the user
+     * @param $bio biography of user
+     */
     function setBio($bio)
     {
         $this->bio = $bio;
     }
-
+    /**
+     * @return biography of user
+     */
     function getBio()
     {
         return $this->bio;
--- classes/PremiumMember.php
+++ classes/PremiumMember.php
@@ -6,23 +6,43 @@
  * Time: 5:10 PM
  */
 
+/**
+ * Class Premium Member represents a premium member of the dating site that extends member
+ */
 class PremiumMember extends Member
 {
     private $_inDoorActivities= array();
     private $_outDoorActivities = array();
 
+    /**
+     * @return array of indoor activities
+     */
     function getIndoorActivities()
     {
         return $this->_inDoorActivities;
     }
+
+    /**
+     * Sets the indoor activities of premium member
+     * @param $inDoorActivities
+     */
     function  setIndoorActivities($inDoorActivities)
     {
         $this->_inDoorActivities = $inDoorActivities;
     }
+
+    /**
+     * @return array of outdoor activities
+     */
     function getOutDoorActivities()
     {
         return $this->_outDoorActivities;
     }
+
+    /**
+     * Sets the outdoor activities of premium member
+     * @param $outDoorActivities
+     */
     function setOutDoorActivities($outDoorActivities)
     {
         $this->_outDoorActivities = $outDoorActivities;
